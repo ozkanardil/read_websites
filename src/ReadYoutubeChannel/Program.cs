0add4bcf2d3fe330898ddd61c5696160f914ed7e
Ozkan <ozkanardil@gmail.com>
2024-06-30 18:16:49 +0200

    YouTube channel video listing project has been created

---
+++ src/ReadYoutubeChannel/Program.cs
@@ -0,0 +1,9 @@
+﻿using Shared;
+
+Console.WriteLine("Read One TouTube Channel!");
+
+string url = "https://www.youtube.com/feeds/videos.xml?user=GoogleDevelopers";
+
+ReadYouTube myClass = new ReadYouTube();
+
+myClass.Read(url, 3);
---
+++ src/Shared/ReadYouTube.cs
@@ -0,0 +1,35 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace Shared
+{
+    public class ReadYouTube
+    {
+        public void Read(string url, int daysBefore)
+        {
+            using var reader = XmlReader.Create(url);
+            var feed = SyndicationFeed.Load(reader);
+
+            DateTime yesterday = DateTime.Now.Subtract(TimeSpan.FromDays(daysBefore));
+
+            if (feed == null)
+            {
+                Console.WriteLine("No video found!");
+            }
+            else
+            {
+                foreach (var post in feed.Items)
+                {
+                    if (post.PublishDate > yesterday)
+                    {
+                        Console.WriteLine("---------------------------------------------------------------");
+                        Console.WriteLine("Title: " + post.Title.Text);
+                        Console.WriteLine("Link: " + post.Links[0].Uri.AbsoluteUri);
+                        Console.WriteLine("PublishDate: " + post.PublishDate);
+                        Console.WriteLine("");
+                    }
+                }
+            }
+        }
+    }
+}
